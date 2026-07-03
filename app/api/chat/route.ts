91f11215f4b3dcc08852b764a94956693716fd46
Yash Mittal <yashmittalmm@gmail.com>
2026-07-03 23:37:42 +0530

    chore: Add tool to save information about user

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,22 +1,42 @@
+import { createResource } from '@/lib/actions/resources';
 import {
   convertToModelMessages,
   createUIMessageStreamResponse,
   streamText,
+  tool,
   toUIMessageStream,
   UIMessage,
 } from 'ai';
+import { z } from 'zod';
 import { createAmazonBedrock  } from "@ai-sdk/amazon-bedrock";
 
 const bedrockMantle = createAmazonBedrock({
   region: 'us-east-1'
 });
 
+export const maxDuration = 30;
+
 export async function POST(req: Request) {
   const { messages }: { messages: UIMessage[] } = await req.json();
 
   const result = streamText({
     model: bedrockMantle("qwen.qwen3-coder-next"),
+    system: `You are a helpful assistant. Check your knowledge base before answering any questions.
+    Only respond to questions using information from tool calls.
+    if no relevant information is found in the tool calls, respond, "Sorry, I don't know."`,
     messages: await convertToModelMessages(messages),
+    tools: {
+      addResource: tool({
+        description: `add a resource to your knowledge base.
+          If the user provides a random piece of knowledge unprompted, use this tool without asking for confirmation.`,
+        inputSchema: z.object({
+          content: z
+            .string()
+            .describe('the content or resource to add to the knowledge base'),
+        }),
+        execute: async ({ content }) => createResource({ content }),
+      }),
+    },
   });
 
   return createUIMessageStreamResponse({
